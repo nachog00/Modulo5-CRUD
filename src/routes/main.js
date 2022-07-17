c9ede1e055e5472723f6e1e9a8e46d7583e8dd2a
nachog00 <ignaciog00@gmail.com>
2022-07-17 16:27:05 -0300

    comente todas las lineas 'a rellenar' para que no se rompiese el servidor.

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // ************ Controller Require ************
 const mainController = require('../controllers/mainController');
 
-router.???('/', mainController.index); 
-router.???('/search', mainController.search); 
+// router.???('/', mainController.index); 
+// router.???('/search', mainController.search); 
 
 module.exports = router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -6,23 +6,23 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 
 /*** GET ALL PRODUCTS ***/ 
-router.???('/', productsController.index); 
+// router.???('/', productsController.index); 
 
 /*** CREATE ONE PRODUCT ***/ 
-router.???('/???/', productsController.create); 
-router.???('/', productsController.store); 
+// router.???('/???/', productsController.create); 
+// router.???('/', productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
-router.???('/:id/', productsController.detail); 
+// router.???('/:id/', productsController.detail); 
 
 /*** EDIT ONE PRODUCT ***/ 
-router.???('/:id/???', productsController.edit); 
-router.???('/:id', productsController.update); 
+// router.???('/:id/???', productsController.edit); 
+// router.???('/:id', productsController.update); 
 
 
 /*** DELETE ONE PRODUCT***/ 
-router.???('/:id', productsController.destroy); 
+// router.???('/:id', productsController.destroy); 
 
 
 module.exports = router;
