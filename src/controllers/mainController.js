761cfc54932064f33c921f1b326cbd8e353a9629
nachog00 <ignaciog00@gmail.com>
2022-07-17 17:04:21 -0300

    controller updated. Fixed missing {product} parameters at index view includes of product-box

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -8,7 +8,12 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
-		res.render('index', {})
+		res.render('index', {
+			products: {
+				'visited' : products.filter(x => x.category === 'visited'),
+				'in-sale' : products.filter(x => x.category === 'in-sale'),
+			}
+		} )
 	},
 	search: (req, res) => {
 		// Do the magic
