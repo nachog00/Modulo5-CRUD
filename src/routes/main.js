2cb4db6ef6b2f5dfb549264b491bcc2a048ec96d
nachog00 <ignaciog00@gmail.com>
2022-07-17 16:33:57 -0300

    fixed routing for get '/', added initial controller logic. Products still not showing and view is still static

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // ************ Controller Require ************
 const mainController = require('../controllers/mainController');
 
-// router.???('/', mainController.index); 
+router.get('/', mainController.index); 
 // router.???('/search', mainController.search); 
 
 module.exports = router;
